b537394cdfa34c80d852d66cd9925befdc1b1f41
PaoNar <pen.castro@yavirac.edu.ec>
2020-09-22 16:31:36 -0500

    pantalla1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+
+export class HomePage {
+  
+  constructor(
+    private splashScreen: SplashScreen,
+    private platform: Platform,
+    private statusBar: StatusBar) {
+
+      this.iniciarApp();
+    }
+
+    iniciarApp(){
+      this.platform.ready()
+      .then(() =>{
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+      })
+    }
 
 }
+
+
